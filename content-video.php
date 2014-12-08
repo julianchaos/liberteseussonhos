24daa3a26a5b293495e09c8a5423de25bf3499c0
Julian Andrade <julianchaos@gmail.com>
2014-12-07 22:47:52 -0200

    Vídeo - Ajuste na faixa de "Em construção"

--- content-video.php
+++ content-video.php
@@ -9,10 +9,10 @@
 ?>
 <section id="page_<?php the_id() ?>" class="content-video page_<?php the_id() ?>">
 	<div class="container">
-		
+		<img src="<?php bloginfo('template_directory') ?>/images/em-construcao.png" class="em-construcao" />
 		<div class="iframe">
 			<iframe width="640" height="360" src="//www.youtube.com/embed/<?php the_field('video_id') ?>?showinfo=0" frameborder="0" allowfullscreen></iframe>
 		</div>
-		<img src="<?php bloginfo('template_directory') ?>/images/video-bg.png" />
 	</div>
 </section>
+<img src="<?php bloginfo('template_directory') ?>/images/video-bg.png" class="bg container" />
